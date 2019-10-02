3de5bd4cdd7f6fbf486c61c954e368766e22233c
DavidArruda <deh0_@hotmail.com>
2019-10-02 15:20:22 -0300

    Criação servlet Autenticar

---
+++ src/servlet/ServletAutenticacao.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import user.UserLogado;
+
+@WebServlet("/pages/ServletAutenticacao")
+public class ServletAutenticacao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ServletAutenticacao() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		if (login.equals("admin") && senha.equals("admin")) {
+			
+			UserLogado logado = new UserLogado();
+			logado.setLogin(login);
+			logado.setSenha(senha);
+			
+			//adiciona Usuario logado a sessao
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+			session.setAttribute("usuario", logado);
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/acessoAoSistema.jsp");
+			dispatcher.forward(request, response);
+
+		}else { //redireciona para a pagina login novamente
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/autenticar.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
