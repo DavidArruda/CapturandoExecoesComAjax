c610f476bd2d25395822eb2e8bcb87b955a32bb8
DavidArruda <deh0_@hotmail.com>
2019-11-25 16:24:36 -0200

    adicionado cabeçalho para dowload de imagens

--- src/servlet/FileUpload.java
+++ src/servlet/FileUpload.java
@@ -19,7 +19,7 @@ import dao.DaoImagem;
 @WebServlet("/pages/fileUpload")
 public class FileUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	DaoImagem daoImagem = new DaoImagem();
 
 	public FileUpload() {
@@ -29,79 +29,74 @@ public class FileUpload extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		try {
-			
-		String acao = request.getParameter("acao");
-		
-		if(acao.equalsIgnoreCase("carregar")) {
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher("upload.jsp");
-			request.setAttribute("listaUserImagem", daoImagem.listar());
-			dispatcher.forward(request, response);
-			
-		}else if(acao.equalsIgnoreCase("download")){
-			
-			String codImagem = request.getParameter("codImagem");
-			String imagem = daoImagem.buscarImagem( codImagem);
-			
-			if (imagem != null) {
-				
-				//Pega a base64 sem o contentType
-				String imagemPura = imagem.split(",")[1];
-				
-				//converte base64 em bytes
-				byte[] imagemBytes = new Base64().decode(imagemPura);
-				
-				//coloca os bytes em um objeto de entrada para processar 
-				InputStream is = new ByteArrayInputStream(imagemBytes);
-				
-				//inicio - escrever os dados na resposta
-				int read = 0;
-				byte[] bytes = new byte[1024];
-				
-				OutputStream os = response.getOutputStream();
-				
-				while((read = is.read(bytes)) != -1) {
-					os.write(bytes,0, read);
+
+			String acao = request.getParameter("acao");
+
+			if (acao.equalsIgnoreCase("carregar")) {
+
+				RequestDispatcher dispatcher = request.getRequestDispatcher("upload.jsp");
+				request.setAttribute("listaUserImagem", daoImagem.listar());
+				dispatcher.forward(request, response);
+
+			} else if (acao.equalsIgnoreCase("download")) {
+
+				String codImagem = request.getParameter("codImagem");
+				String imagem = daoImagem.buscarImagem(codImagem);
+
+				if (imagem != null) {
+					
+					response.setHeader("Content-disposition","attachment;file-name-imagem.png");
+
+					// Pega a base64 sem o contentType
+					String imagemPura = imagem.split(",")[1];
+
+					// converte base64 em bytes
+					byte[] imagemBytes = new Base64().decode(imagemPura);
+
+					// coloca os bytes em um objeto de entrada para processar
+					InputStream is = new ByteArrayInputStream(imagemBytes);
+
+					// inicio - escrever os dados na resposta
+					int read = 0;
+					byte[] bytes = new byte[1024];
+
+					OutputStream os = response.getOutputStream();
+
+					while ((read = is.read(bytes)) != -1) {
+						os.write(bytes, 0, read);
+					}
+
+					os.flush();
+					os.close();
+
+					// fim escrever os dados na resposta
+
 				}
-				
-				os.flush();
-				os.close();
-				
-				//fim escrever os dados na resposta
-				
-				
-				
 			}
-		}
-		
-		
-		
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		
+
 		try {
-			
-		
-		// usar variavel fileupload para salvar no banco
-		String fileUpload = request.getParameter("fileUpload");
-		System.out.println(fileUpload);
-		
-		daoImagem.gravarImagem(fileUpload);
-		
-		response.getWriter().write("Upload realizado com sucesso");
-		
+
+			// usar variavel fileupload para salvar no banco
+			String fileUpload = request.getParameter("fileUpload");
+			System.out.println(fileUpload);
+
+			daoImagem.gravarImagem(fileUpload);
+
+			response.getWriter().write("Upload realizado com sucesso");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 
 	}
 
