f090da7e82a76b5963044d3fc26249dc20769c0e
DavidArruda <deh0_@hotmail.com>
2019-11-25 09:37:11 -0200

    adicionado atributo codImagem e seus respectivos metodos get e set

---
+++ src/entidades/BeanImagem.java
@@ -0,0 +1,24 @@
+package entidades;
+
+public class BeanImagem {
+	
+	private Long codImagem;
+	private String imagem;
+	
+	public Long getCodImagem() {
+		return codImagem;
+	}
+	
+	public void setCodImagem(Long codImagem) {
+		this.codImagem = codImagem;
+	}
+	
+	public String getImagem() {
+		return imagem;
+	}
+	
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+}
