b42ab8da59e68214089452da6e31eec9ea1d73d7
DavidArruda <deh0_@hotmail.com>
2019-11-25 09:36:31 -0200

    adicionado metodo para buscar imagem por id

--- src/dao/DaoImagem.java
+++ src/dao/DaoImagem.java
@@ -2,9 +2,13 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import connection.SingleConnection;
+import entidades.BeanImagem;
 
 public class DaoImagem {
 	
@@ -30,9 +34,46 @@ public class DaoImagem {
 		
 			e.printStackTrace();
 		}
+	}
+	
+	public List<BeanImagem> listar() throws Exception {
+		List<BeanImagem> lista = new ArrayList<BeanImagem>();
+
+		String sql = "select * from imagem";
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+		ResultSet resultSet = statement.executeQuery();
+
+		while (resultSet.next()) {
+
+			BeanImagem img = new BeanImagem();
+			img.setCodImagem(resultSet.getLong("codImagem"));
+			img.setImagem(resultSet.getString("imagem"));
+			
+			lista.add(img);
+
+		}
+		return lista;
+	}
+
+	public String buscarImagem(String codImagem) throws SQLException {
 		
+		try {
 		
+			String sql = "select imagem from imagem where codImagem = " + codImagem;
+			
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet resultSet = statement.executeQuery();
+			
+			while(resultSet.next()) {
+				return resultSet.getString("imagem");
+			}
 		
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return null;
 	}
 	
 	
