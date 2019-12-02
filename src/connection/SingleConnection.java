ddac0fbc5488c656e2251379dac0369759906132
DavidArruda <deh0_@hotmail.com>
2019-12-02 17:02:38 -0200

    Modificao do banco

--- src/connection/SingleConnection.java
+++ src/connection/SingleConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class SingleConnection {
 	
-	private static String banco = "jdbc:postgresql://localhost:5432/curso-jsp?autoReconnect=true";
+	private static String banco = "jdbc:postgresql://localhost:5432/datas?autoReconnect=true";
 	private static String password = "admin";
 	private static String user = "postgres";
 	private static Connection connection = null;
