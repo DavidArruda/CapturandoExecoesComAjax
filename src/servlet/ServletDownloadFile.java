4c256d834788320a8df9ad5a9fbca9f3a0d9233d
DavidArruda <deh0_@hotmail.com>
2019-12-11 16:27:08 -0200

    Modificado lista generica para uma lista de usuarios

--- src/servlet/ServletDownloadFile.java
+++ src/servlet/ServletDownloadFile.java
@@ -22,12 +22,12 @@ import services.RelatorioService;
 /**
  * Servlet implementation class ServletDownloadFile
  */
-@WebServlet("/ServletDownloadFile")
+@WebServlet("/pages/ServletDownloadFile")
 public class ServletDownloadFile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private RelatorioService relatorioService = new RelatorioService();
-	private DaoUsuario daoUsuario = new DaoUsuario(); 
+	private DaoUsuario daoUsuario = new DaoUsuario();
 
 	public ServletDownloadFile() {
 		super();
@@ -37,61 +37,62 @@ public class ServletDownloadFile extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String tipoExportar = request.getParameter("tipoExportar");
 		
+
 		try {
-		
+
 			ServletContext context = request.getServletContext();
 			
-		List<Usuario> usuarios = daoUsuario.getsuarios();
-		
-		List dados = new ArrayList();
-		dados.add(usuarios);
-		
-		String fileUrl = relatorioService.gerarRelatorio(dados, new HashMap(), "rel_usuario", "rel_usuario", context);
-		
-		//Construir o caminho completo e absoluto do arquivo
-		File downloadFile = new File(fileUrl);
-		
-		FileInputStream inputStream = new FileInputStream(downloadFile);
-		
-		//Obter o tipo MIME do arquivo
-		
-		String mimeType = context.getMimeType(fileUrl);
-		
-		if(mimeType == null) {
-			//define como tipo binario se mapeamento nao for encontrado
-			mimeType = "application/octet-stream";
-		}
-		
-		//define atributos para resposta
-		response.setContentType(mimeType);
-		response.setContentLength((int) downloadFile.length());
-		
-		//definir cabeçalho para resposta
-		
-		String headerKey = "Content-Disposition";
-		String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
-		
-		response.setHeader(headerKey, headerValue);
-		
-		//obter fluxo de saida da resposta
-		OutputStream outputStream = response.getOutputStream();
-		
-		byte[] buffer = new byte[4096];
-		
-		int bytesReader = -1;
-		
-		//escrever bytes lidos apartir do fluxo de entrada para o fluxo de saida
-		
-		while((bytesReader = inputStream.read(buffer)) != -1){
-			outputStream.write(buffer,0, bytesReader);
-		}
-		
-		inputStream.close();
-		outputStream.close();
-		
-		}catch (Exception e) {
+			String tipoExportar = request.getParameter("tipoExportar");
+			
+			
+			List<Usuario> usuarios = daoUsuario.getsuarios();
+
+			String fileUrl = relatorioService.gerarRelatorio(usuarios, new HashMap(), "rel_usuario", "rel_usuario",
+					context);
+
+			// Construir o caminho completo e absoluto do arquivo
+			File downloadFile = new File(fileUrl);
+
+			FileInputStream inputStream = new FileInputStream(downloadFile);
+
+			// Obter o tipo MIME do arquivo
+
+			String mimeType = context.getMimeType(fileUrl);
+
+			if (mimeType == null) {
+				// define como tipo binario se mapeamento nao for encontrado
+				mimeType = "application/octet-stream";
+			}
+
+			// define atributos para resposta
+			response.setContentType(mimeType);
+			response.setContentLength((int) downloadFile.length());
+
+			// definir cabeçalho para resposta
+
+			String headerKey = "Content-Disposition";
+			String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+
+			response.setHeader(headerKey, headerValue);
+
+			// obter fluxo de saida da resposta
+			OutputStream outputStream = response.getOutputStream();
+
+			byte[] buffer = new byte[4096];
+
+			int bytesReader = -1;
+
+			// escrever bytes lidos apartir do fluxo de entrada para o fluxo de saida
+
+			while ((bytesReader = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesReader);
+			}
+
+			inputStream.close();
+			outputStream.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
