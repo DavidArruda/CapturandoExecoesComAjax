01ca48149cafce5ea09d2e8dd98eba762d5490a6
DavidArruda <deh0_@hotmail.com>
2019-12-13 16:34:33 -0200

    Remoção de import não utilizado

--- src/dao/DaoUsuario.java
+++ src/dao/DaoUsuario.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import connection.SingleConnection;
 import connection.SingleConnection2;
 import entidades.Usuario;
 
